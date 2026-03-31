fe36600078085982192637f0f652b1f1f8291b2d
Jaiden Rothrock <jaiden_rothrock@hotmail.com>
2026-03-30 21:24:55 -0500

    switched out pottery array for separate functions untilizing the makePottery funciton

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,11 @@
 // Imports go first
 
-import { makePottery } from "./PotteryWheel.js"
+import { firePottery } from "./Kiln.js"
+import { makePottery } from "./potteryWheel.js"
 
 // Make 5 pieces of pottery at the wheel
 
+/*
 const potteries = [
     {
         shape: "mug",
@@ -31,17 +33,28 @@ const potteries = [
         height: 1
     }
 ]
+*/
 
+let mug = makePottery("mug", 1, 4)
 
-//initial test for loop
-for (const pottery of potteries) {
-    let i = makePottery(pottery["shape"], pottery["weight"], pottery["height"])
-    console.log(i)
-}
+let platter = makePottery("Platter", 1, 1)
 
+let burjKhalifa = makePottery("Burj Khalifa", 1000000, 40000)
+
+let bowl = makePottery("Bowl", 1, 3)
+
+let dinnerPlate = makePottery("Dinner Plate", 1, 1)
 
 // Fire each piece of pottery in the kiln
 
+console.log(firePottery(mug, 2010))
+
+/*
+for (const pottery of potteries) {
+    let i = firePottery(pottery["shape"], 2200)
+    console.log(i)
+}
+*/
 
 // Determine which ones should be sold, and their price
 
