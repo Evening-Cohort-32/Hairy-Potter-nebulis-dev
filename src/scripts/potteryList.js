11419f6480552c358495cf9f495fc2119f7f92c8
Jaiden Rothrock <jaiden_rothrock@hotmail.com>
2026-03-31 19:03:27 -0500

    fixed a typo

--- src/scripts/potteryList.js
+++ src/scripts/potteryList.js
@@ -8,7 +8,7 @@ export const potteryList = () => {
          <section class="pottery" id="pottery--1">
             <h2 class="pottery__shape">${pottery.shape}</h2>
                 <div class="pottery__properties">
-                <p>Item weighs ${pottery.weight} grams and is ${pottery.height} cm in height</p>
+                <p>Item weighs ${pottery.weight} gram(s) and is ${pottery.height} cm in height</p>
                 </div>
                 <div class="pottery__price">Price is $${pottery.price}</div>
          </section>`
